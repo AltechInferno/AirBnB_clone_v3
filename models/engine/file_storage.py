d1dbe0a20b355ca4f839f3dbbc3b8e2af968c4ff
Altech Inferno <altechinferno@gmail.com>
2023-10-31 18:29:10 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-Contains the FileStorage class
+FileStorage class
 """
 
 import json
+import models
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -12,12 +13,12 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"User": User, "BaseModel": BaseModel, "Amenity": Amenity,
+           "Place": Place, "Review": Review, "State": State, "City": City}
 
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
+    """serializes & deserializes to instances"""
 
     # string - path to the JSON file
     __file_path = "file.json"
@@ -25,59 +26,59 @@ class FileStorage:
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """returns dictionary __objects"""
         if cls is not None:
-            new_dict = {}
+            new_dt = {}
             for key, value in self.__objects.items():
                 if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
+                    new_dt[key] = value
+            return new_dt
         return self.__objects
 
-    def get(self, cls, id):
-        """retrieve one object"""
-        if cls and id:
-            object = models.storage.all(cls)
-            for i, j in object.items():
-                if j.id == id:
-                    return j
-        return None
-
-    def count(self, cls=None):
-        """count the number of objects in storage"""
-        return len(models.storage.all(cls))
-
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """sets __objects the obj with key <obj class name>.id"""
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        json_objects = {}
+        """serializes __objects to (path: __file_path)"""
+        json_obj = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            json_obj[key] = self.__objects[key].to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """deserializes JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                j_obj = json.load(f)
+            for key in j_obj:
+                self.__objects[key] = classes[j_obj[key]["__class__"]](**j_obj[key])
         except:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """delete obj from __objects"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return len(models.storage.all(cls))
+
+    def get(self, cls, id):
+        """A method to retrieve an object"""
+        if cls and id:
+            object = models.storage.all(cls)
+            for i, j in object.items():
+                if j.id == id:
+                    return j
+        return None
